e2f6ad25e7249e7301835a099b51444834ccbff6
Nolan Gericke <nolan.gericke@icloud.com>
2025-08-20 00:50:54 -0400

    Created HabitListView

---
+++ Habits/Views/HabitListView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitListView.swift
+//  Habits
+//
+//  Created by Nolan Gericke on 8/20/25.
+//
+
+import SwiftUI
+
+struct HabitListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitListView()
+}
